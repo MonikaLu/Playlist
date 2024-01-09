df5c83463840304fb6096641e3f0d2b410b89bad
Monika Luu <monika-luu@hotmail.com>
2024-01-09 14:56:04 +0100

    Refactor code

--- getSongNamesFromMp3Files.py
+++ getSongNamesFromMp3Files.py
@@ -1,14 +1,44 @@
 import os
+import re
 
 dir = 'music'
 song_names_file = 'songNames.csv'
+deviants_file = 'songNotFound.csv'
 
-with open(song_names_file, 'w') as file:
-    file.write('Songs:' + '\n')
-    for filename in os.listdir(dir):
-        if filename.endswith(".mp3"):
-            file.write(filename.replace('.mp3', '') + '\n')
+unused_words = ['audio', 'lyrics', '[FULL Audio]', 'MP3', '(lyrics)', '+']
 
-with open("songNotFound.csv", 'w') as file:
-    file.write('Songs:' + '\n')
-    file.close()
+def remove_chars_from_strings(text):
+    pattern = r'[\[\]\(\)]'
+    text = re.sub(pattern, '', text)
+    for word in unused_words:
+        if text.__contains__(word):
+            text = text.replace(word, '')
+    return text
+    
+
+# def add_names_to_csv():
+#     with open(song_names_file, 'w') as file:
+#         file.write('Songs:' + '\n')
+#         for filename in os.listdir(dir):
+#             if filename.endswith(".mp3") and filename.__contains__("-"):
+#                 file.write(filename.replace('.mp3', '') + '\n')
+#         file.close()
+
+# def add_deviants_to_csv():
+#     with open(deviants_file, 'w') as file:
+#         file.write('Songs:' + '\n')
+#         for filename in os.listdir(dir):
+#             file.write(filename.replace('.mp3', '') + '\n')
+#         file.close()
+        
+def add_songs_to_csv():
+    with open(song_names_file, 'w') as normal_names, open(deviants_file, 'w') as deviants:
+        for filename in os.listdir(dir):
+            if remove_chars_from_strings(filename).endswith(".mp3") and filename.__contains__("-"):
+                normal_names.write(remove_chars_from_strings(filename).replace('.mp3', '') + '\n')
+            else:
+                deviants.write(remove_chars_from_strings(filename).replace('.mp3', '') + '\n')
+        normal_names.close()
+        deviants.close()
+
+add_songs_to_csv()
