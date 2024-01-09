7560f12751f16ff9b4884cef77dbaee76313db99
Monika Luu <monika-luu@hotmail.com>
2024-01-09 18:47:09 +0100

    Refine code

--- addSongsToPlaylist.py
+++ addSongsToPlaylist.py
@@ -9,11 +9,6 @@ load_dotenv(os.path.join(BASEDIR, '.env.local'))
 access_token=os.getenv('AUTHORIZATION')
 playlistId=os.getenv('PLAYLIST_ID')
 base_url = 'https://api.spotify.com/v1/search'
-
-# Data
-songNamesFromFile = pd.read_csv('songNames.csv')
-songNames = songNamesFromFile['Songs:'].tolist()
-
 headers = {
     'Authorization': f'Bearer {access_token}',
     'Content-Type': 'application/json'
@@ -27,39 +22,50 @@ def get_playlist_tracks(playlist_id, headers):
     existing_track_ids = [item['track']['id'] for item in track_data['items']]
     return existing_track_ids
 
-existing_ids = get_playlist_tracks(playlistId, headers)
+def getSongIds(songList):
+    song_ids = []
+    for song in songList:
+        params = {
+            'q': song,
+            'type': 'track',
+            'limit': 1
+        }
+        res = requests.get(base_url, headers=headers, params=params).json()
+        tracks = res['tracks']['items']
+        if tracks:
+            song_ids.append(tracks[0]['id'])
+    return song_ids
 
-# q and type are defined by Spotify and required.
-song_ids = []
-for song in songNames:
-    params = {
-        'q': song,
-        'type': 'track',
-        'limit': 1
-    }
-    res = requests.get(base_url, headers=headers, params=params).json()
-    tracks = res['tracks']['items']
-    if tracks:
-        song_ids.append(tracks[0]['id'])
-        
 # Compare song_ids with existing_ids
-filtered_ids = []
-for songId in song_ids:
-    if songId not in existing_ids:
-        filtered_ids.append(songId)
+def getFilteredList(songList, existing_ids):
+    filtered_ids = []
+    for songId in songList:
+        if songId not in existing_ids:
+            filtered_ids.append(songId)
+    return filtered_ids
 
-songs_successfully_added = 0    
+def fetchSongs(songList):
+    filtered_ids = getFilteredList(songList)
+    songs_successfully_added = 0
+    for id in filtered_ids:
+        add_tracks_response = requests.post(
+        f'https://api.spotify.com/v1/playlists/{playlistId}/tracks',
+        headers=headers,
+        json={'uris': [f'spotify:track:{id}']})
+        if add_tracks_response.status_code == 201:
+            songs_successfully_added += 1
+            print(songs_successfully_added, "/", len(filtered_ids), end='\r')
+        if songs_successfully_added == len(filtered_ids):
+            print("Completed!")
+        if songs_successfully_added == 0:
+            print("No new songs are added!")
 
-for id in filtered_ids:
-    add_tracks_response = requests.post(
-    f'https://api.spotify.com/v1/playlists/{playlistId}/tracks',
-    headers=headers,
-    json={'uris': [f'spotify:track:{id}']})
-    if add_tracks_response.status_code == 201:
-       songs_successfully_added += 1
-       print(songs_successfully_added, "/", len(filtered_ids), "added!", end='\r')
-    if songs_successfully_added == len(filtered_ids):
-        print("Completed!")
-
-if songs_successfully_added == 0:
-    print("No new songs are added!")
+# Data
+def main():
+    chunk_size = 10  # Adjust based on your system's capabilities
+    existing_ids = get_playlist_tracks(playlistId, headers)
+    for chunk in pd.read_csv('songNames.csv', chunksize=chunk_size):
+        songNames = chunk['Songs:'].tolist()
+        fetchSongs(getFilteredList(getSongIds(songNames), existing_ids))
+        
+main()
--- getSongNamesFromMp3Files.py
+++ getSongNamesFromMp3Files.py
@@ -5,7 +5,7 @@ dir = 'music'
 song_names_file = 'songNames.csv'
 deviants_file = 'songNotFound.csv'
 
-unused_words = ['audio', 'lyrics', '[FULL Audio]', 'MP3', '(lyrics)', '+', 'Audio', 'Lyrics', 'album', 'AUDIO']
+unused_words = ['audio', 'lyrics', '[FULL Audio]', 'MP3', '(lyrics)', '+', 'Audio', 'Lyrics', 'album', 'AUDIO', 'Album', 'mini', 'Mini']
 
 def remove_chars_from_strings(text):
     pattern = r'[\[\]\(\)]'
@@ -17,6 +17,8 @@ def remove_chars_from_strings(text):
         
 def add_songs_to_csv():
     with open(song_names_file, 'w') as normal_names, open(deviants_file, 'w') as deviants:
+        normal_names.write("Songs:" + '\n')
+        deviants.write("Songs:" + '\n')
         for filename in os.listdir(dir):
             if remove_chars_from_strings(filename).endswith(".mp3") and filename.__contains__("-"):
                 normal_names.write(remove_chars_from_strings(filename).replace('.mp3', '') + '\n')
